580125fdd3408336f4cd033ed07a2cfbb9ad24a2
Andrew House <andrewhouse621@gmail.com>
2014-08-15 11:20:41 -0400

    actually did it this time v2

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -15,7 +15,7 @@ class GamesController < ApplicationController
   def create
     @game = Game.new(game_params)
     if @game.save!
-      redirect_to :index
+      redirect_to root_path
     else
       render :new
     end
